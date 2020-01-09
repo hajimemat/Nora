f8a37ccfb5adedd68e3b5e3503f75ce620e10c76
Hajime MATSUMOTO <hajime.matsumoto@avap.co.jp>
2020-01-10 05:13:03 +0900

    update

--- tests/Dotenv/DotenvTest.php
+++ tests/Dotenv/DotenvTest.php
@@ -7,23 +7,33 @@ use PHPUnit\Framework\TestCase;
 class DotenvTest extends TestCase
 {
     /**
-     * 起動テスト
+     * @test
      */
-    public function testCreate()
+    public function ファイルの読み込みテスト()
     {
         $dotenv = (new NewDotenv)(dirname(__DIR__));
         $originalUser = getenv('USER');
         $dotenv->load();
         $this->assertSame($originalUser, getenv('USER'));
+    }
+
+    /**
+     * @test
+     */
+    public function 環境変数のオーバライド()
+    {
+        $originalUser = getenv('USER');
+        $dotenv = (new NewDotenv)(dirname(__DIR__));
+        $originalUser = getenv('USER');
         $dotenv->override();
         $this->assertNotEquals($originalUser, getenv('USER'));
         $this->assertSame("hajime", getenv('USER'));
     }
 
     /**
-     * .envファイルが存在しなければ例外を投げる
+     * @test
      */
-    public function testError()
+    public function ファイルが存在しなければ例外()
     {
         $this->expectException(EnvFileNotFound::class);
         $dotenv = (new NewDotenv)(__DIR__);
